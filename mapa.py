c1302a56c0bfe8ab0941509b9cf385f67545aed8
anjosh <joshuaan1@gmail.com>
2013-11-24 11:43:06 -0500

    initial

---
+++ mapa.py
@@ -0,0 +1,63 @@
+import psycopg2
+
+dbname = "hackdown"
+username = "crosschx_hack"
+password = "Cr0sschx_H4ck"
+host = "hack.crosschx.com"
+
+conn = psycopg2.connect(database=dbname, user=username, password=password, host=host)
+
+cur = conn.cursor()
+
+departments = ["Pediatrics", "Emergency", "Cardiology", "Gynaecology", "Neurology", "Radiology"]
+
+cur.execute("SELECT distinct t.location FROM transaction as t;")
+
+hospitals = cur.fetchall()
+total_hospitals = len(hospitals)
+
+new_json = open('./new.json', 'w')
+
+new_json.write('{"name": "CrossChx",\n "children": [\n')
+
+for hospital_no in range(0, total_hospitals):
+	hospital = hospitals[hospital_no]
+	hospital_name = hospital[0]
+	data = '{"name": "' + hospital_name + '",\n' + '"children": [\n'
+	cur.execute("Select t.operation, count(t.encounter_id) FROM transaction as t Where t.location = (%s) GROUP BY t.operation", [hospital_name])
+	transactions = cur.fetchall()
+	total_transactions = len(transactions)
+	for i in range(0, total_transactions):
+		transaction = transactions[i]
+		transaction_type = transaction[0]
+		transaction_amount = transaction[1]
+		data += '{"name": "' + transaction_type + '",\n' + '"children": [\n'
+		cur.execute("Select p.first_name, p.last_name, tt.operation_time FROM transaction as t, patient as p, transaction_time as tt Where t.location = (%s) and p.puid = t.puid and t.operation = (%s) and t.encounter_id = tt.encounter_id", [hospital_name, transaction_type])
+		patients = cur.fetchall()
+		total_patients = (len(patients)^(i*6))/90
+		for x in range(0, total_patients):
+			patient = patients[x*2]
+			first_name = patient[0]
+			last_name = patient[1]
+			encounters = int(patient[2][1:])*133%31
+			patient_name = first_name + ' ' + last_name
+			data += '{"name": "' + patient_name + '", ' + '"size": ' + str(encounters) + '}'
+			if x != total_patients-1:
+				data += ','
+			data += '\n'
+			
+		data += ']\n'
+		data += '}'
+		if i != total_transactions-1:
+			data += ',\n'
+
+	data += ']\n'
+	data += '}'
+	if hospital_no != total_hospitals-1:
+		data += ',\n'
+	new_json.write(data)
+
+new_json.write(']}')
+
+cur.close()
+conn.close()
---
+++ test.js
@@ -0,0 +1,100 @@
+var pg = require('pg');
+var d3 = require("d3");
+
+var patients = [];
+
+var username = "crosschx_hack";
+var password = "Cr0sschx_H4ck";
+var database = "hackdown";
+var host = "hack.crosschx.com";
+var port = "5432";
+
+var conString = "postgres://" + username + ':' + password + '@' + host + ':' + port + '/' + database;
+
+var client = new pg.Client(conString);
+client.connect();
+
+
+var query = client.query('SELECT puid, first_name, last_name FROM patient');
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    json_data = JSON.stringify(result.rows, null, "    ")
+    console.log(json_data);
+    client.end();
+});
+
+// d3 code
+
+var margin = 10,
+    outerDiameter = 960,
+    innerDiameter = outerDiameter - margin - margin;
+
+var x = d3.scale.linear()
+    .range([0, innerDiameter]);
+
+var y = d3.scale.linear()
+    .range([0, innerDiameter]);
+
+var color = d3.scale.linear()
+    .domain([-1, 5])
+    .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+    .interpolate(d3.interpolateHcl);
+
+var pack = d3.layout.pack()
+    .padding(2)
+    .size([innerDiameter, innerDiameter])
+    .value(function(d) { return d.size; })
+
+var svg = d3.select("body").append("svg")
+    .attr("width", outerDiameter)
+    .attr("height", outerDiameter)
+  .append("g")
+    .attr("transform", "translate(" + margin + "," + margin + ")");
+
+d3.json(patients, function(error, root) {
+  var focus = root,
+      nodes = pack.nodes(root);
+
+  svg.append("g").selectAll("circle")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", function(d) { return d.parent ? d.children ? "node" : "node node--leaf" : "node node--root"; })
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .attr("r", function(d) { return d.r; })
+      .style("fill", function(d) { return d.children ? color(d.depth) : null; })
+      .on("click", function(d) { return zoom(focus == d ? root : d); });
+
+  svg.append("g").selectAll("text")
+      .data(nodes)
+    .enter().append("text")
+      .attr("class", "label")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+      .style("opacity", function(d) { return d.parent === root ? 1 : 0; })
+      .text(function(d) { return d.name; });
+
+  d3.select(window)
+      .on("click", function() { zoom(root); });
+
+  function zoom(d, i) {
+    focus = d;
+
+    var k = innerDiameter / d.r / 2;
+    x.domain([d.x - d.r, d.x + d.r]);
+    y.domain([d.y - d.r, d.y + d.r]);
+    d3.event.stopPropagation();
+
+    var transition = d3.selectAll("text,circle").transition()
+        .duration(d3.event.altKey ? 7500 : 750)
+        .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+    transition.filter("circle")
+        .attr("r", function(d) { return k * d.r; });
+
+    transition.filter("text")
+        .style("opacity", function(d) { return d.parent === focus ? 1 : 0; });
+  }
+});
+
+d3.select(self.frameElement).style("height", outerDiameter + "px");
